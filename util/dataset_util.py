c1327881a467e1305112361c9a2ee7d95762f6cd
Valeria Sakovskaya <valeria.sakovskaya@bell-sw.com>
2024-02-16 12:39:06 +0400

    Added a clustering notebook

--- util/dataset_util.py
+++ util/dataset_util.py
@@ -1,19 +1,20 @@
-import os
 import glob
-import pandas as pd
-import numpy as np
-import seaborn as sns
+import os
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 
 def get_data_from_csv(csv_dir: str, goals):
     """Parse each summary.csv file stored in `csv_dir`.
     Directory `csv_dir` shall contain summary files
-    that describe performance metrics for certain 
+    that describe performance metrics for certain
     JVM flags.
 
     E.g., summary_4_12.csv describes performance metrics
-    (avgGCPause; 0.0014; seconds) for the JVM with 
+    (avgGCPause; 0.0014; seconds) for the JVM with
     flag_1=4 and flag_2=12.
     Args:
         csv_dir (str):  Path to a directory that contains
@@ -21,41 +22,41 @@ def get_data_from_csv(csv_dir: str, goals):
         goal (list):     A list of key names.
     """
     assert goals is not None, "`goals` should be a list of strings, not None"
-    sep = ';'
+    sep = ";"
     flag_1_values = []
     flag_2_values = []
     goal_values = []
 
     for summary in glob.glob(csv_dir):
         basename = os.path.splitext(summary)[0]
-        p, m = basename.split('_')[-2], basename.split('_')[-1]
+        p, m = basename.split("_")[-2], basename.split("_")[-1]
         summary_abs_path = os.path.abspath(summary)
 
-        summary_df = (pd
-                      .read_csv(
-                          summary_abs_path, sep=sep, skiprows=1, header=None)
-                      .replace(',', '', regex=True)
-                      .replace('n.a.', 'NaN', regex=True))
-        # params = summary_df[summary_df[0].isin(goals)][1].astype(float).values
-        params = []
-        for goal in goals:
-            param = summary_df[summary_df[0] == goal][1].astype(float).values[0]
-            params.append(param)
+        summary_df = pd.read_csv(
+            summary_abs_path, sep=sep, skiprows=1, header=None
+        ).replace(",", "", regex=True)
+        summary_df.iloc[:, 1] = summary_df.iloc[:, 1].replace("n.a.", "NaN", regex=True)
+        params = (
+            summary_df[summary_df.iloc[:, 0].isin(goals)]
+            .iloc[:, 1]
+            .values.astype(np.float32)
+        )
         assert len(goals) == len(params), "Check if goal name is in summary"
-        
+
         flag_1_values.append(int(p))
         flag_2_values.append(int(m))
         goal_values.append(params)
 
     return flag_1_values, flag_2_values, goal_values
 
+
 def read_single_log(key, filename):
-  """bench: avg"""
-  (name, n, avg) = (None, 0, 0.00)
-  with open(filename) as fd:
-    for ln in fd:
-      if ln.startswith(key):
-        f = ln.split(",")
-        (name, n, avg) = (f[1], n + 1, avg + float(f[3]))
-  avg = avg/n if n > 0 else 0
-  return (name, avg)
+    """bench: avg"""
+    (name, n, avg) = (None, 0, 0.00)
+    with open(filename) as fd:
+        for ln in fd:
+            if ln.startswith(key):
+                f = ln.split(",")
+                (name, n, avg) = (f[1], n + 1, avg + float(f[3]))
+    avg = avg / n if n > 0 else 0
+    return (name, avg)
